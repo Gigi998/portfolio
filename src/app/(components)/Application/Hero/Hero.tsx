a842ae76bca9315686ea35b26f965afdffb6b108
Luigi <luigicode2@gmail.com>
2025-01-29 11:49:46 +0100

    fix build issues

--- src/app/(components)/Application/Hero/Hero.tsx
+++ src/app/(components)/Application/Hero/Hero.tsx
@@ -11,12 +11,19 @@ const Hero = () => {
         Full Stack Developer
       </div>
       <p className='text-body-1 text-secondary sm:mt-5 mt-2 max-w-[400px] sm:max-w-full'>
-        I'm really passionate about software development 💻 and
-        finance/investing 📈.
-        <br /> I love building full-stack apps with tools like React, Next.js,
-        Node.js, and NestJS 🚀.
-        <br /> Lately, I’ve been diving into blockchain and Web3🔗, exploring
-        how decentralized tech can shape the future.
+        {`I'm really passionate about software development 💻 and
+          finance/investing 📈.`}
+        <br />
+        {`
+          I love building full-stack apps with tools like React, Next.js,
+          Node.js, and NestJS 🚀.
+        `}
+        <br />
+        {`
+          
+          Lately, I’ve been diving into blockchain and Web3🔗, exploring how
+          decentralized tech can shape the future.
+        `}
       </p>
     </div>
   );
--- src/app/(components)/Basic/Button/Button.tsx
+++ src/app/(components)/Basic/Button/Button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MouseEventHandler, ReactElement } from "react";
+import { MouseEvent, ReactElement } from "react";
 import { twMerge } from "tailwind-merge";
 import { Type } from "./constants";
 
@@ -10,7 +10,9 @@ export interface Props {
   startIcon?: ReactElement;
   endIcon?: ReactElement;
   disabled?: boolean;
-  onClick?: MouseEventHandler;
+  onClick?: (
+    e: MouseEvent<HTMLAnchorElement> | MouseEvent<HTMLButtonElement>
+  ) => void;
   className?: string;
   href?: string;
   target?: string;
@@ -39,9 +41,11 @@ const Button = ({
 
   const generateButtonsAttributes = () => {
     return {
-      onClick: (e: any) => onClick?.(e),
+      onClick: (
+        e: MouseEvent<HTMLAnchorElement> | MouseEvent<HTMLButtonElement>
+      ) => onClick?.(e),
       className: twMerge(
-        "text-primary px-4 py-3 flex items-center justify-center gap-x-2 border-2 border-invert rounded-full h-10 text-[18px] leading-[22px] shadow-[0_2px_0_0_#121212] hover:bg-blue",
+        "text-primary px-4 py-3 flex items-center justify-center gap-x-2 border-2 border-invert rounded-full h-10 text-[18px] leading-[22px] hover:bg-blue",
         className
       ),
       disabled: disabled,
